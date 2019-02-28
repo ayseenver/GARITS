61d27ef2062d03c4c70ca43f918a46d261859e40
Ayse Enver <ayse.enver@city.ac.uk>
2019-02-28 20:38:31 +0000

    updated GUI package names

--- src/teamproject/GUI/Customers.java
+++ src/teamproject/GUI/Customers.java
@@ -3,8 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package teamproject;
-
+package teamproject.GUI;
 /**
  *
  * @author ahmetsesli
