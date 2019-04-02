64f650ab976f486c8b071a2324548e7e0ed7d2a9
Ahmet Sesli <ahmetsesli245@gmail.com>
2019-04-03 00:12:47 +0100

    tested customer list

--- src/teamproject/GUI/UpdateCustomer.java
+++ src/teamproject/GUI/UpdateCustomer.java
@@ -756,17 +756,17 @@ public class UpdateCustomer extends javax.swing.JPanel {
         checkBoxConfigurePayLater.setFont(new java.awt.Font("Lucida Grande", 0, 14)); // NOI18N
         checkBoxConfigurePayLater.setText("Pay Later Option");
         accountHolderPane.add(checkBoxConfigurePayLater);
-        checkBoxConfigurePayLater.setBounds(10, 0, 133, 27);
+        checkBoxConfigurePayLater.setBounds(10, 0, 144, 23);
 
         checkBoxDiscountPlan.setFont(new java.awt.Font("Lucida Grande", 0, 15)); // NOI18N
         checkBoxDiscountPlan.setText("Discount plan");
         accountHolderPane.add(checkBoxDiscountPlan);
-        checkBoxDiscountPlan.setBounds(10, 30, 115, 29);
+        checkBoxDiscountPlan.setBounds(10, 30, 131, 24);
 
         labelDiscountDetail.setFont(new java.awt.Font("Lucida Grande", 0, 14)); // NOI18N
         labelDiscountDetail.setText("Details:");
         accountHolderPane.add(labelDiscountDetail);
-        labelDiscountDetail.setBounds(30, 70, 47, 20);
+        labelDiscountDetail.setBounds(30, 70, 50, 20);
 
         comboBoxDiscountPlan.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "None", "Fixed", "Variable", "Flexible" }));
         comboBoxDiscountPlan.addActionListener(new java.awt.event.ActionListener() {
@@ -775,14 +775,14 @@ public class UpdateCustomer extends javax.swing.JPanel {
             }
         });
         accountHolderPane.add(comboBoxDiscountPlan);
-        comboBoxDiscountPlan.setBounds(140, 30, 63, 20);
+        comboBoxDiscountPlan.setBounds(140, 30, 140, 27);
 
         jScrollPane7.setViewportView(listBusinessType);
 
         accountHolderPane.add(jScrollPane7);
         jScrollPane7.setBounds(80, 70, 200, 120);
         accountHolderPane.add(textFieldPercentage);
-        textFieldPercentage.setBounds(280, 160, 40, 20);
+        textFieldPercentage.setBounds(280, 160, 40, 26);
 
         buttonSetDiscountPlan.setText("Set");
         buttonSetDiscountPlan.addActionListener(new java.awt.event.ActionListener() {
@@ -796,7 +796,7 @@ public class UpdateCustomer extends javax.swing.JPanel {
         labelPercentage.setFont(new java.awt.Font("Lucida Grande", 0, 14)); // NOI18N
         labelPercentage.setText("%");
         accountHolderPane.add(labelPercentage);
-        labelPercentage.setBounds(320, 160, 12, 20);
+        labelPercentage.setBounds(320, 160, 9, 20);
 
         jPanel1.add(accountHolderPane, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 240, 420, 230));
 
