a3cdaab5c11638a0b9317a386d133ccfd9785f28
Ahmet Sesli <ahmetsesli245@gmail.com>
2019-04-04 00:31:58 +0100

    Buttons

--- src/teamproject/GUI/PartSale.java
+++ src/teamproject/GUI/PartSale.java
@@ -384,7 +384,7 @@ public class PartSale extends javax.swing.JPanel {
                 buttonChangeQuantityActionPerformed(evt);
             }
         });
-        jPanel1.add(buttonChangeQuantity, new org.netbeans.lib.awtextra.AbsoluteConstraints(1070, 600, -1, -1));
+        jPanel1.add(buttonChangeQuantity, new org.netbeans.lib.awtextra.AbsoluteConstraints(1070, 590, -1, -1));
 
         textFieldQuantity.setFont(new java.awt.Font("Lucida Grande", 0, 18)); // NOI18N
         textFieldQuantity.addActionListener(new java.awt.event.ActionListener() {
@@ -392,7 +392,7 @@ public class PartSale extends javax.swing.JPanel {
                 textFieldQuantityActionPerformed(evt);
             }
         });
-        jPanel1.add(textFieldQuantity, new org.netbeans.lib.awtextra.AbsoluteConstraints(1040, 600, 30, 30));
+        jPanel1.add(textFieldQuantity, new org.netbeans.lib.awtextra.AbsoluteConstraints(1040, 590, 30, 30));
 
         buttonRemove.setText("Remove");
         buttonRemove.addActionListener(new java.awt.event.ActionListener() {
@@ -400,7 +400,7 @@ public class PartSale extends javax.swing.JPanel {
                 buttonRemoveActionPerformed(evt);
             }
         });
-        jPanel1.add(buttonRemove, new org.netbeans.lib.awtextra.AbsoluteConstraints(1070, 630, 150, -1));
+        jPanel1.add(buttonRemove, new org.netbeans.lib.awtextra.AbsoluteConstraints(1070, 620, 150, 30));
 
         buttonProduceInvoice.setFont(new java.awt.Font("Lucida Grande", 0, 18)); // NOI18N
         buttonProduceInvoice.setText("Produce Invoice");
